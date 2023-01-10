8e59ecb10394bee48487e4319be703c5f40e8986
Ubuntu <ubuntu@weedomics-vm-1.novalocal>
2023-01-10 06:09:18 +0000

    piling up

--- work/18/2b28b1fe2c82e288e65c1d5b35815e/.command.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo 'Fix the reference genome format.'
-mv /data-weedomics-1/align_fastq/test/ref/Lolium_rigidum_genome.fasta /data-weedomics-1/align_fastq/test/ref/Lolium_rigidum_genome.fasta.bk
-python3 /data-weedomics-1/align_fastq/modules/../scripts/fix_reference_genome_format.py         /data-weedomics-1/align_fastq/test/ref/Lolium_rigidum_genome.fasta.bk         /data-weedomics-1/align_fastq/test/ref/Lolium_rigidum_genome.fasta
--- work/6d/9089344ed8ec17e730ea58202680bd/.command.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo 'Index the reference genome for samtools.'
-samtools faidx         /data-weedomics-1/align_fastq/test/ref/Lolium_rigidum_genome.fasta         -@ 32
-
-echo "Output:"
-echo "  (1/1) {reference_genome}.fai"
